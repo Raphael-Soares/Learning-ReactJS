cfa0e1089808c53c47e7b04f5ae48d63eb885839
Raphael-Soares <soaresphaell@gmail.com>
2022-07-26 15:23:19 -0300

    Estruturação do projeto

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,8 +1,34 @@
+//Imports
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+//Componentes
+import Navbar from "./components/Navbar";
+
+//Layouts
+
+import Container from "./layouts/Container";
+
+//Views
+import Home from "./views/Home";
+import Contato from "./views/Contato";
+import Empresa from "./views/Empresa";
+import NovoProjeto from "./views/NovoProjeto";
+
 function App() {
     return (
         <div className="App">
-            <h1>Olá, Mundo! </h1>
-            <p>Costs</p>
+            <Router>
+                <Navbar></Navbar>
+                <Container customClass="min-height">
+                    <Routes>
+                        <Route exact path="/" element={<Home />}></Route>
+                        <Route path="/contato" element={<Contato />}></Route>
+                        <Route path="/empresa" element={<Empresa />}></Route>
+                        <Route path="projeto" element={<NovoProjeto />}></Route>
+                    </Routes>
+                </Container>
+                <p>Footer</p>
+            </Router>
         </div>
     );
 }
---
+++ projeto/src/views/NovoProjeto.js
@@ -0,0 +1,9 @@
+function NovoProject() {
+    return (
+        <div>
+            <h2>Novo Project</h2>
+        </div>
+    );
+}
+
+export default NovoProject;
