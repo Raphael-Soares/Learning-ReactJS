d9d82d51cdd672b20ccef42198c7b08af5c10072
Raphael-Soares <soaresphaell@gmail.com>
2022-07-26 09:57:09 -0300

    Project

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -2,7 +2,7 @@ function App() {
     return (
         <div className="App">
             <h1>Olá, Mundo! </h1>
-            <p>Costsssssssssssss</p>
+            <p>Costs</p>
         </div>
     );
 }
